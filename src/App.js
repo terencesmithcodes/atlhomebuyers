1332ee809f91fd0b2f6039e545933fe3f1c33b2c
Terence Smith <terencesmith1@me.com>
2023-11-29 22:04:12 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
     <>
       <div className="App">
         <Routes>
-          <Route path="/atlhomebuyers" element={<Atlhomebuyers />} />
+          <Route path="/atlcapitalventures.com" element={<Atlhomebuyers />} />
           <Route path="/privacy" element={<Privacy />} />
         </Routes>
       </div>
